f783c902955a21c7bf88d2c0ffa7d1282469edc2
tigerza117 <noppawanza1171@gmail.com>
2023-03-01 16:36:23 +0700

    ⚡️ improve test performance

--- test_netmikolab.py
+++ test_netmikolab.py
@@ -1,22 +1,56 @@
+import threading
+
 from netmikolab import *
 
 
+def validate_ip(param, tests):
+    for test in tests:
+        assert get_ip(param, test[0]) == test[1]
+
+
+def validate_subnet(param, tests):
+    for test in tests:
+        assert get_subnet(param, test[0]) == test[1]
+
+
+def validate_description_and_stat(param, tests):
+    for test in tests:
+        data = get_iface_stat(param, test[0])
+        assert data == test[1]
+
+
 def test_ip():
+    threads = list()
     for device in devices_config.values():
         param = dict(params, ip=device["mgmt_ip"])
-        for test in device["tests"]["iface_ip"]:
-            assert get_ip(param, test[0]) == test[1]
+        x = threading.Thread(target=validate_ip, args=(param, device["tests"]["iface_ip"]))
+        threads.append(x)
+        x.start()
+
+    for index, thread in enumerate(threads):
+        thread.join()
 
 
 def test_subnet():
+    threads = list()
     for device in devices_config.values():
         param = dict(params, ip=device["mgmt_ip"])
-        for test in device["tests"]["iface_subnet"]:
-            assert get_subnet(param, test[0]) == test[1]
+        x = threading.Thread(target=validate_subnet, args=(param, device["tests"]["iface_subnet"]))
+        threads.append(x)
+        x.start()
+
+    for index, thread in enumerate(threads):
+        thread.join()
 
 
 def test_description_and_stat():
+    threads = list()
     for device in devices_config.values():
         param = dict(params, ip=device["mgmt_ip"])
-        for test in device["tests"]["iface_description_and_stat"]:
-            assert get_iface_stat(param, test[0]) == test[1]
+        x = threading.Thread(target=validate_description_and_stat,
+                             args=(param, device["tests"]["iface_description_and_stat"]))
+        threads.append(x)
+        x.start()
+
+    for index, thread in enumerate(threads):
+        thread.join()
